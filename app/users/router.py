036b182e33046a545138b19fb2a2e6fff383f063
Shakzod Usmanov <shakhzodusmanov2@gmail.com>
2025-03-04 21:16:34 +0100

    coock

--- app/users/router.py
+++ app/users/router.py
@@ -66,8 +66,8 @@ async def register_user(user_data: SUserAuth):
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     access_token = create_access_token({"sub": str(user.id)})
-    response.set_cookie("booking_access_token", access_token, httponly=False,
-                        samesite="none", secure=False)
+    response.set_cookie("booking_access_token", access_token, httponly=True,
+                        )
     return {"access_token": access_token}
 
 
